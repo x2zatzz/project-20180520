0e7a3d215d9f82f1bd7a8f6925cb5f8f1c6150d3
Michael James Albatana <dev.mjta@gmail.com>
2018-06-02 10:44:34 +0800

    DEV v0.0.0.0t optimized logic for brand-name system

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -1,11 +1,17 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Item;
 
 $factory->define(App\Item::class, function (Faker $faker) {
   $price = rand(1, 9)*1000 + rand(1, 9)*100 + rand(1, 9)*10 + rand(1, 9)*1 + rand(1, 100)*0.01;
   $brand = $faker->word;
   $name = $faker->unique()->word;
+  if(Item::all()->firstWhere('namebrand',"$brand-$name")===null){
+    $namebrand = "$brand-$name";
+  } else{
+    $namebrand = $faker->unique()->word . '-' . $faker->word;
+  }
 
   return [
     'name' => $name,
--- database/migrations/2018_05_26_233344_create_items_table.php
+++ database/migrations/2018_05_26_233344_create_items_table.php
@@ -21,7 +21,7 @@ class CreateItemsTable extends Migration
 
       $table->string('name');
       $table->string('brand');
-      $table->string('namebrand');
+      $table->string('namebrand')->unique();
       $table->string('model');
       $table->longText('description')->nullable();
 
